4ef9796516d86d3f4cb368b75d2638047e1e334a
ismetozhn <ismetozhn@hotmail.com>
2024-02-02 19:07:32 +0300

    sign out process

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React from 'react';
+import { auth } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function HomeScreen() {
+
+  const navigation = useNavigation();
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate('Login')
+      })
+      .catch((error) => alert(error.message));
+
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Çıkış Yap</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  button: {
+    backgroundColor: '#405490',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 20,
+    width: '60%',
+    marginTop: 40
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+})
